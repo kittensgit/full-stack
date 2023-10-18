9b29ebb6a21bfc76c66cd8292b8937ef0dddd588
nika <nikaonime@gmail.com>
2023-10-18 09:56:06 +0300

    save user, bcrypt

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 import { validationResult } from 'express-validator';
+import bcrypt from 'bcrypt';
 import { registerValidator } from './validations/auth.js';
+import UserModel from './models/user.js';
 
 mongoose
     .connect(
@@ -19,15 +21,25 @@ const app = express(); // создвем експресс приложение
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidator, (req, res) => {
+app.post('/auth/register', registerValidator, async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json(errors.array());
     }
 
-    res.json({
-        success: true,
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        passwordHash,
     });
+
+    const user = await doc.save();
+
+    res.json(user);
 });
 
 app.listen(2222, (err) => {
