c04fddbf361b977e6ec1a96b42ae2d9a81a7e657
nika <nikaonime@gmail.com>
2023-10-18 07:32:08 +0300

    test login, app.use(express.json())

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import express from 'express';
 
 const app = express(); // создвем експресс приложение
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('hello, customer!!');
 });
 
 app.post('/auth/login', (req, res) => {
+    console.log(req.body);
     res.json({
         success: true,
     });
@@ -16,6 +19,5 @@ app.listen(2222, (err) => {
     if (err) {
         console.log(err);
     }
-
     console.log('server run');
 });
