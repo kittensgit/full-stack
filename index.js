0b7b82d35ebe65266ea02a33b010c0840ca7aa58
nika <nikaonime@gmail.com>
2023-10-25 12:30:41 +0300

    jwt token, hide the user's password from the resp

--- index.js
+++ index.js
@@ -30,17 +30,30 @@ app.post('/auth/register', registerValidation, async (req, res) => {
 
         const password = req.body.password;
         const salt = await bcrypt.genSalt(10);
-        const passwordHash = await bcrypt.hash(password, salt);
+        const hash = await bcrypt.hash(password, salt);
 
         const doc = new UserModel({
             email: req.body.email,
             fullName: req.body.fullName,
-            passwordHash,
+            passwordHash: hash,
         });
 
         const user = await doc.save();
 
-        res.json(user);
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            },
+            'secret123',
+            { expiresIn: '30d' }
+        );
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            userData,
+            token,
+        });
     } catch (err) {
         console.log(err);
         res.status(500).json({
