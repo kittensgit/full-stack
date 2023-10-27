a0a90d65efecc39911367cc3c0b00bc5bc1f14bf
nika <nikaonime@gmail.com>
2023-10-27 10:34:17 +0300

    CRUD(PostController)

---
+++ controller/PostController.js
@@ -0,0 +1,110 @@
+import PostModel from '../models/post.js';
+
+export const getAll = async (req, res) => {
+    try {
+        const posts = await PostModel.find().populate('user').exec();
+
+        res.json(posts);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Failed to get all articles',
+        });
+    }
+};
+export const getOne = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        const doc = await PostModel.findOneAndUpdate(
+            { _id: postId },
+            { $inc: { viewsCount: 1 } },
+            { returnDocument: 'after' }
+        );
+
+        if (!doc) {
+            return res.status(404).json({
+                message: 'Article not found',
+            });
+        }
+
+        res.json(doc);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Failed to get article',
+        });
+    }
+};
+export const remove = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        const doc = await PostModel.findByIdAndDelete({
+            _id: postId,
+        });
+
+        if (!doc) {
+            res.status(500).json({
+                message: 'Article not found',
+            });
+        }
+
+        res.json({
+            success: true,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Failed to remove article',
+        });
+    }
+};
+
+export const create = async (req, res) => {
+    try {
+        const doc = new PostModel({
+            title: req.body.title,
+            text: req.body.text,
+            tags: req.body.tags,
+            imageUrl: req.body.imageUrl,
+            user: req.userId,
+        });
+
+        const post = await doc.save();
+
+        res.json(post);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Failed to create article',
+        });
+    }
+};
+
+export const update = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        await PostModel.updateOne(
+            {
+                _id: postId,
+            },
+            {
+                title: req.body.title,
+                text: req.body.text,
+                tags: req.body.tags,
+                imageUrl: req.body.imageUrl,
+                user: req.userId,
+            }
+        );
+        res.json({
+            success: true,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Failed to update article',
+        });
+    }
+};
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { registerValidation } from './validations/auth.js';
+import {
+    registerValidation,
+    loginValidation,
+    postCreateValidation,
+} from './validations.js';
 import checkAuth from './utils/checkAuth.js';
 import * as UserController from './controller/UserController.js';
+import * as PostController from './controller/PostController.js';
 
 mongoose
     .connect(
@@ -19,11 +24,15 @@ const app = express(); // создвем експресс приложение
 
 app.use(express.json());
 
-app.post('/auth/login', UserController.login);
-
+app.post('/auth/login', loginValidation, UserController.login);
 app.post('/auth/register', registerValidation, UserController.register);
-
 app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, PostController.update);
 
 app.listen(2222, (err) => {
     if (err) {
